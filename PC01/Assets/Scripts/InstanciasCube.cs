42702307fc27cfb1ecee2c680270fa08cfae7533
deenglow <dvidsen@gmail.com>
2023-09-19 00:55:24 +0200

    Personalizando el proyecto

---
+++ PC01/Assets/Scripts/InstanciasCube.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+public class InstanciasCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ScriptableObjectsInGame scriptableObjectsInGame;
+    void Start()
+    {
+        for (int i = 0; i < scriptableObjectsInGame.numeroPrefabRespawn; i++)
+        {
+            Instantiate(scriptableObjectsInGame.prefab, transform.position, Quaternion.identity);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PC01/Assets/Scripts/ScriptableObjectsInGame.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/ScriptableObjectsInGame", order = 1)]
+public class ScriptableObjectsInGame : ScriptableObject
+{
+    public GameObject prefab;
+    public int numeroPrefabRespawn;
+    
+}
